ff1f75b655b6a0e8d9f241e19e94ff3e874d79b5
CSMancapa <cmancapa@gmail.com>
2023-04-26 10:19:04 +0000

    A script that starts a web flask application

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,25 +1,21 @@
-#!/usr/bin/env python3
-"""
-This module defines a Flask web application with two routes.
-"""
-
+#!/usr/bin/python3
+""" Module for running a Flask web application """
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """
-    Returns a greeting message.
-    """
-    return 'Hello HBNB!'
+    """ Displays 'Hello HBNB!' """
+    return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """
-    Returns a message with the acronym HBNB.
-    """
-    return 'HBNB'
+    """ Displays 'HBNB' """
+    return "HBNB"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
